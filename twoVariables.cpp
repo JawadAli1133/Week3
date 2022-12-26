fb057f0bd1f97c629a8a2d36a1254e3569cd67f1
JawadAli1133 <120698315+JawadAli1133@users.noreply.github.com>
2022-12-26 07:16:13 -0800

    Add files via upload

---
+++ twoVariables.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+main()
+{
+
+int numOne;
+int numTwo;
+numTwo = 0;
+
+cout << "Enter first number: ";
+cin >> numOne;
+
+numTwo = numTwo + numOne;
+
+cout << "Enter second number: ";
+cin >> numOne;
+
+numTwo = numTwo + numOne;
+
+cout << "Enter third number: ";
+cin >> numOne;
+
+numTwo = numTwo + numOne;
+
+cout << "Enter fourth number: ";
+cin >> numTwo;
+
+numTwo = numTwo + numOne;
+
+cout << "Enter fifth number: ";
+cin >> numOne;
+
+numTwo =numTwo +numOne;
+
+cout << "Sum of 5 numbers is: " << numTwo ;
+
+}
+
